36182a37f49fb620e0fd6c56d8b71799c1ea65df
Chukwuka Emi <emichukwuka@gmail.com>
2021-08-11 00:05:04 +0100

    added single feed page

--- components/Messaging.jsx
+++ components/Messaging.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Button, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-		width: "100%",
-	},
+  root: {
+    flexGrow: 1,
+    width: "100%",
+  },
 }));
 
 const Messaging = () => {
-	const classes = useStyles();
-	return <div className={classes.root}></div>;
+  const classes = useStyles();
+  return <div className={classes.root}>Message component</div>;
 };
 
 export default Messaging;
