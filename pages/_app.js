1146a2a9da1a2ee9aeaa62b49864e30d93e2cdcf
Chukwuka Emi <emichukwuka@gmail.com>
2021-10-31 12:24:50 +0100

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -20,14 +20,6 @@ export default function App({ Component, pageProps }) {
         tool: ["console", "elements"],
         container: document.body,
       });
-      var src = "//cdn.jsdelivr.net/npm/eruda";
-      if (
-        !/eruda=true/.test(window.location) &&
-        localStorage.getItem("active-eruda") != "true"
-      )
-        return;
-      document.write("<scr" + 'ipt src="' + src + '"></scr' + "ipt>");
-      document.write("<scr" + "ipt>eruda.init();</scr" + "ipt>");
     })();
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
